f7dc7d46d9834f8172b856beffaa67f98e2cf45d
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-07-04 15:21:48 -0300

    create

---
+++ index.js
@@ -0,0 +1,14 @@
+/*array = [1,2,3,4,5]
+
+function filtraPares(array){
+    new_array = []
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            new_array.push(array[i])
+        }
+    }
+    return new_array
+}
+
+resultado = filtraPares(array)
+console.log(resultado); */
